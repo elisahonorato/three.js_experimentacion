7ce79c50765ab82015334f90d88a6e2cd21854e9
Elisa Honorato <elisahonoratolira@uc.cl>
2022-04-19 14:19:21 -0400

    dist

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app= express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(__dirname + '/dist/index.html');
 })
 
 app.listen(3000, ()=> {
