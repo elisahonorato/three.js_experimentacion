a9feac216a4d752d6e07b9c8ceee2262cc23a45e
Elisa Honorato <elisahonoratolira@uc.cl>
2022-04-19 14:36:05 -0400

    gh pages

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app= express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/dist/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 })
 
 app.listen(3000, ()=> {
