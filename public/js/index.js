893b27fa6891a5742a26767f3a05f68ede779068
Elisa Honorato <elisahonoratolira@uc.cl>
2022-04-19 16:32:09 -0400

    scale

--- public/js/index.js
+++ public/js/index.js
@@ -14,7 +14,7 @@ function init(){
     camera = new THREE.PerspectiveCamera(
         75,
         window.innerWidth/ window.innerHeight,
-        0.1,
+        0.01,
         1000
     );
     camera.position.z = 10;
@@ -80,7 +80,7 @@ function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight
     camera.updateProjectionMatrix()
     renderer.setSize(window.innerWidth, window.innerHeight)
-    renderer.render()
+    render()
 }
 
 
